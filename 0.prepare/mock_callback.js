1954f7813443bcee7802d29ebfe44f942df05cc5
Asako Kondo <kondoasako@gmail.com>
2020-03-09 12:51:30 +0900

    Revert "Prepare"

--- 0.prepare/mock_callback.js
+++
@@ -1,9 +0,0 @@
-// myFetchはサーバにリクエストを送る処理と想定
-myFetch('example1.txt', function(err, result1){
-  myFetch('example2.txt', function(err, result2){
-    myFetch('example3.txt', function(err, result3){
-      // すべての処理が終わったあとの処理
-    });
-  });
-});
-// それぞれのコールバックの中でのエラー処理を考慮した結果、可読性が下がってしまう例
