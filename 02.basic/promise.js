ca373577740a9bf7bed3a969a2f0a3645c489641
asakon <kondoasako@gmail.com>
2020-03-09 15:41:58 +0900

    Promise.then

--- 02.basic/promise.js
+++ 02.basic/promise.js
@@ -6,3 +6,9 @@ var promise = new Promise(function(resolve, reject) {
     reject("処理が失敗しました");
   }
 });
+
+promise.then(function(result) {
+  console.log("成功時の処理");
+}, function(err) {
+  console.log("失敗時の処理");
+});
